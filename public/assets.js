4e8f3489c95c7cf8f82e91d037a49fc3ef1c2faa
unknown <ade12peace@gmail.com>
2026-01-02 21:09:31 +0100

    dev

--- public/assets.js
+++ public/assets.js
@@ -24,7 +24,6 @@ import logo_light from './logo_light.png'
 import blog_icon from './blog_icon.png'
 import add_icon from './add_icon.png'
 import email_icon from './email_icon.png'
-import upload_area from './upload_area.png'
 
 export const assets = {
     facebook_icon,
